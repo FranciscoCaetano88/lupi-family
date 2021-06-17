423b9e57cfa3b50becaaf761eca6f96342d76aa2
FranciscoCaetano88 <falc1988@gmail.com>
2021-06-17 20:45:09 +0100

    populate with family members

--- src/assets/members/index.js
+++ src/assets/members/index.js
@@ -1,29 +1,137 @@
-import miguelAngeloLupi from './miguel-angelo-lupi.json';
+import adelaideLupi from './adelaide-lupi.json';
+import aldaDoCoutoLupi from './alda-do-couto-lupi.json';
+import anaPintoBastoLupi from './ana-pinto-basto-lupi.json';
+import antonioDosSantosJorge from './antónio-dos-santos-jorge.json';
+import antonioLupiNogueira from './antónio-lupi-nogueira.json';
+import augustoPintoDeAraujo from './augusto-pinto-de-araújo.json';
 import bandeiraDeMelo from './bandeira-de-melo.json';
-import bernardaAmeliaDasDores from './bernarda-amélia-das-dores.json';
+import bernardaAmeliaDasDores from './bernarda-amélia-das-dores.json';
+import bertaMarquesDaCosta from './berta-marques-da-costa.json';
 import carlotaLupi from './carlota-lupi.json';
+import domingosDaSilvaNogueira from './domingos-da-silva-nogueira.json';
+import domingosLupiNogueira from './domingos-lupi-nogueira.json';
+import eduardoBastoLupi from './eduardo-basto-lupi.json';
+import eduardoDoCoutoLupi from './eduardo-do-couto-lupi.json';
+import eduardoJoaoDaCostaLupi from './eduardo-joão-da-costa-lupi.json';
+import eduardoLupi from './eduardo-lupi.json';
+import emiliaLupiNogueira from './emília-lupi-nogueira.json';
+import fernandoNogueiraDeAraújo from './fernando-nogueira-de-araújo.json';
 import franciscoLupi from './francisco-lupi.json';
-import joaoEvangelistaLupi from './joão-evangelista-lupi.json';
-import joaquimJoseDoNascimento from './joaquim-josé-do-nascimento-lupi.json';
-import joseMariaMelchiadesLupi from './josé-maria-melchiades-lupi.json';
-import mariaCandidaCorrea from './maria-cândida-corrêa.json';
-import mariaJoseLupi from './maria-josé-lupi.json';
+import fredericoLupiNogueira from './frederico-lupi-nogueira.json';
+import fredericoPintoBastoLupi from './frederico-pinto-basto-lupi.json';
+import joaquimJoseDoNascimentoLupi from './joaquim-josé-do-nascimento-lupi.json';
+import joaquinaDosSantos from './joaquina-dos-santos.json';
+import jorgeLupi from './jorge-lupi.json';
+import joseLupi from './josé-lupi.json';
+import joseMariaDosSantos from './josé-maria-dos-santos.json';
+import joseMariaMelchiadesLupi from './josé-maria-melchiades-lupi.json';
+import joseSamuelLupi from './josé-samuel-lupi.json';
+import joaoCardosoMata from './joão-cardoso-mata.json';
+import joaoEvangelistaLupi from './joão-evangelista-lupi.json';
+import joaoPauloFreire from './joão-paulo-freire.json';
+import judithLupiFreire from './judith-lupi-freire.json';
+import judithLupiNogueira from './judith-lupi-nogueira.json';
+import judithNogueiraDeAraujo from './judith-nogueira-de-araújo.json';
+import julioNogueiraDeAraujo from './júlio-nogueira-de-araújo.json';
+import leonorPintoBastoLupi from './leonor-pinto-basto-lupi.json';
+import luísRiosDeOliveiraAlves from './luís-rios-de-oliveira-alves.json';
+import margaridaDuysensLupi from './margarida-duysens-lupi.json';
+import mariaAmaliaPintoBasto from './maria-amália-pinto-basto.json';
+import mariaAmeliaFreitasPereira from './maria-amélia-freitas-pereira.json';
+import mariaCandidaCorrea from './maria-cândida-corrêa.json';
+import mariaCandidaDosSantosLupi from './maria-cândida-dos-santos-lupi.json';
+import mariaCandidaPires from './maria-cândida-pires.json';
+import mariaDeLourdesLupiFreire from './maria-de-lourdes-lupi-freire.json';
+import mariaDeLurdesPereira from './maria-de-lurdes-pereira.json';
+import mariaGertrudesRapousoDoCouto from './maria-gertrudes-rapouso-do-couto.json';
+import mariaInesDoCarmoLupi from './maria-inês-do-carmo-lupi.json';
+import mariaInesLupiNogueira from './maria-inês-lupi-nogueira.json';
+import mariaJoseLupi from './maria-josé-lupi.json';
+import mariaJosePereiraLupi from './maria-josé-pereira-lupi.json';
 import mariaSorianaDoCarmo from './maria-soriana-do-carmo.json';
+import marianaGoes from './mariana-goes.json';
+import miguelAngeloLupi from './miguel-angelo-lupi.json';
+import marioDoCoutoLupi from './mário-do-couto-lupi.json';
+import nunoTristaoNeves from './nuno-tristão-neves.json';
+import raulLupiNogueira from './raul-lupi-nogueira.json';
+import reneeIdaHubertineDuysens from './reneé-ida-hubertine-duysens.json';
+import samuelDosSantosJorge from './samuel-dos-santos-jorge.json';
+import samuelDosSantosLupi from './samuel-dos-santos-lupi.json';
+import samuelLupi from './samuel-lupi.json';
+import saraLupiNogueira from './sara-lupi-nogueira.json';
+import sebastiaoNogueiraDeAraujo from './sebastião-nogueira-de-araújo.json';
+import vascoBlancLupi from './vasco-blanc-lupi.json';
+import vascoDoCoutoLupi from './vasco-do-couto-lupi.json';
 import vicenciaAntolini from './vicencia-antolini.json';
 import vicenteLupi from './vicente-lupi.json';
+import virginiaClara from './virgínia-clara.json';
 
 export default [
     miguelAngeloLupi,
     vicenciaAntolini,
+    adelaideLupi,
+    aldaDoCoutoLupi,
+    anaPintoBastoLupi,
+    antonioDosSantosJorge,
+    antonioLupiNogueira,
+    augustoPintoDeAraujo,
     bandeiraDeMelo,
     bernardaAmeliaDasDores,
+    bertaMarquesDaCosta,
     carlotaLupi,
+    domingosDaSilvaNogueira,
+    domingosLupiNogueira,
+    eduardoBastoLupi,
+    eduardoDoCoutoLupi,
+    eduardoJoaoDaCostaLupi,
+    eduardoLupi,
+    emiliaLupiNogueira,
+    fernandoNogueiraDeAraújo,
     franciscoLupi,
-    joaoEvangelistaLupi,
-    joaquimJoseDoNascimento,
+    fredericoLupiNogueira,
+    fredericoPintoBastoLupi,
+    joaquimJoseDoNascimentoLupi,
+    joaquinaDosSantos,
+    jorgeLupi,
+    joseLupi,
+    joseMariaDosSantos,
     joseMariaMelchiadesLupi,
+    joseSamuelLupi,
+    joaoCardosoMata,
+    joaoEvangelistaLupi,
+    joaoPauloFreire,
+    judithLupiFreire,
+    judithLupiNogueira,
+    judithNogueiraDeAraujo,
+    julioNogueiraDeAraujo,
+    leonorPintoBastoLupi,
+    luísRiosDeOliveiraAlves,
+    margaridaDuysensLupi,
+    mariaAmaliaPintoBasto,
+    mariaAmeliaFreitasPereira,
     mariaCandidaCorrea,
+    mariaCandidaDosSantosLupi,
+    mariaCandidaPires,
+    mariaDeLourdesLupiFreire,
+    mariaDeLurdesPereira,
+    mariaGertrudesRapousoDoCouto,
+    mariaInesDoCarmoLupi,
+    mariaInesLupiNogueira,
     mariaJoseLupi,
+    mariaJosePereiraLupi,
     mariaSorianaDoCarmo,
+    marianaGoes,
+    marioDoCoutoLupi,
+    nunoTristaoNeves,
+    raulLupiNogueira,
+    reneeIdaHubertineDuysens,
+    samuelDosSantosJorge,
+    samuelDosSantosLupi,
+    samuelLupi,
+    saraLupiNogueira,
+    sebastiaoNogueiraDeAraujo,
+    vascoBlancLupi,
+    vascoDoCoutoLupi,
     vicenteLupi,
+    virginiaClara,
 ];
